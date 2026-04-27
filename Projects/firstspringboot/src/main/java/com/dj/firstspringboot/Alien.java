5dcf1af85935e71797091e9f9c14c81105ce0e00
Dhanunjay Jagu <jagudhanunjai@gmail.com>
2026-04-27 15:01:46 +0000

    Implementation of DI - spring creating object

---
+++ Projects/firstspringboot/src/main/java/com/dj/firstspringboot/Alien.java
@@ -0,0 +1,10 @@
+package com.dj.firstspringboot;
+
+import org.springframework.stereotype.Component;
+
+@Component 
+public class Alien {
+    public void code (){
+        System.out.println("Coding");
+    }
+}
