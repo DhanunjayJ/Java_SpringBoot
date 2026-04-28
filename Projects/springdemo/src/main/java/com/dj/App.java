313745446bdb7501f23be84dea58f664a1582dd7
Dhanunjay Jagu <jagudhanunjai@gmail.com>
2026-04-28 14:30:14 +0000

    lazy init and inner bean

--- Projects/springdemo/src/main/java/com/dj/App.java
+++ Projects/springdemo/src/main/java/com/dj/App.java
@@ -9,7 +9,15 @@ public class App
     {
         ApplicationContext context =  new ClassPathXmlApplicationContext("spring.xml");
         
-        Alien obj = (Alien) context.getBean("alien");
+        // Alien obj = (Alien) context.getBean("alien");
+        //another way to get bean
+        Alien obj = context.getBean("alien",Alien.class);
+        //search by type.
+        //you can remove the id in the beam
+        // Desktop obj1 = context.getBean(Desktop.class);
+        //this will give error becaause there are two beans of the type computer. 
+        // so we need to specify primary or name here. 
+        // Computer obj2 = context.getBean(Computer.class);
         System.out.println(obj.getAge());
         obj.code();
 
