4a374078fa1fdffb5db1ae3669bc44e531afb10d
Dhanunjay Jagu <jagudhanunjai@gmail.com>
2026-04-28 10:22:05 +0000

    Constructor injection - types

--- Projects/springdemo/src/main/java/com/dj/Alien.java
+++ Projects/springdemo/src/main/java/com/dj/Alien.java
@@ -8,6 +8,11 @@ public class Alien {
     public int getAge() {
         return age;
     }
+   
+    public Alien(int age, Laptop lap) {
+        this.age = age;
+        this.lap = lap;
+    }
 
     public void setAge(int age) {
         System.out.println("Spring is setting the values using setter");
