0c5fd9d3eac26c6839d0328d0da5eeedbedd682d
Dhanunjay Jagu <jagudhanunjai@gmail.com>
2026-04-29 10:22:44 +0000

    java based config , file creating, Bean,Scope,Autowrite and Qualifier,primary

--- Projects/springdemo/src/main/java/com/dj/App.java
+++ Projects/springdemo/src/main/java/com/dj/App.java
@@ -1,25 +1,37 @@
 package com.dj;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+// import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.dj.config.AppConfig;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        ApplicationContext context =  new ClassPathXmlApplicationContext("spring.xml");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        // Desktop dt = context.getBean(Desktop.class);
+        // dt.compile();
+
+        Alien obj1 = context.getBean(Alien.class);
+        // obj1.setAge(21);
+        System.out.println(obj1.getAge());
+        obj1.code();
         
-        // Alien obj = (Alien) context.getBean("alien");
-        //another way to get bean
-        Alien obj = context.getBean("alien",Alien.class);
-        //search by type.
-        //you can remove the id in the beam
-        // Desktop obj1 = context.getBean(Desktop.class);
-        //this will give error becaause there are two beans of the type computer. 
-        // so we need to specify primary or name here. 
-        // Computer obj2 = context.getBean(Computer.class);
-        System.out.println(obj.getAge());
-        obj.code();
+        // ApplicationContext context =  new ClassPathXmlApplicationContext("spring.xml");
+        // // Alien obj = (Alien) context.getBean("alien");
+        // //another way to get bean
+        // Alien obj = context.getBean("alien",Alien.class);
+        // //search by type.
+        // //you can remove the id in the beam
+        // // Desktop obj1 = context.getBean(Desktop.class);
+        // //this will give error becaause there are two beans of the type computer. 
+        // // so we need to specify primary or name here. 
+        // // Computer obj2 = context.getBean(Computer.class);
+        // System.out.println(obj.getAge());
+        // obj.code();
 
     }
 }
---
+++ Projects/springdemo/src/main/java/com/dj/config/AppConfig.java
@@ -0,0 +1,43 @@
+package com.dj.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.Primary;
+
+import com.dj.Alien;
+import com.dj.Computer;
+import com.dj.Desktop;
+import com.dj.Laptop;
+
+@Configuration
+public class AppConfig {
+    
+    // @Bean (name = {"com2","desktop1","djdesktop",})// bean name is by default the method name
+    @Bean
+    // @Scope("prototype")
+    public Desktop desktop(){
+        return new Desktop();
+    }
+
+    @Bean
+    //when we have two beans of the same type com
+    //it will give error so we need to use @Qualifier
+    //@Autowire here is a not madatory. 
+    public Alien alien(@Qualifier("desktop") @Autowired Computer com){
+        Alien obj = new Alien();
+        obj.setAge(25);
+        obj.setCom(com);
+        return obj;
+    }
+
+    @Bean
+    @Primary // instead of the confusionn 
+    // it will go for the laptop here. 
+    public Laptop laptop(){
+        return new Laptop();
+    }
+
+}
