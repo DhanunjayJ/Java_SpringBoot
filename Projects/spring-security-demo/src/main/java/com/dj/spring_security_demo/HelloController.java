cf83c70b724afa0bd54d736cbfa41c1b4dce54fc
Dhanunjay Jagu <jagudhanunjai@gmail.com>
2026-05-10 16:01:39 +0000

    Setting username and password, getting session id

---
+++ Projects/spring-security-demo/src/main/java/com/dj/spring_security_demo/HelloController.java
@@ -0,0 +1,20 @@
+package com.dj.spring_security_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String greet(HttpServletRequest request){
+        return "Hello World!" + request.getSession().getId();
+    }
+
+    @GetMapping("about")
+    public String about(HttpServletRequest request){
+        return "Dhanunjay Spring Website" + request.getSession().getId();
+    }
+}
