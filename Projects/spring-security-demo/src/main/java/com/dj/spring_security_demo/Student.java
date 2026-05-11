2316145c8d1dc750a2e98f1b4cbe97585b3a1892
Dhanunjay Jagu <jagudhanunjai@gmail.com>
2026-05-11 12:20:08 +0000

    Implmemted CSRF Token fetching

---
+++ Projects/spring-security-demo/src/main/java/com/dj/spring_security_demo/Student.java
@@ -0,0 +1,17 @@
+package com.dj.spring_security_demo;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+    private String tech;
+}
